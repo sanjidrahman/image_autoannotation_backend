ae35d10b27054b2e5df2ec04f668c7ae289f6596
Sanjid Rahman <rahmansanjid505@gmail.com>
2024-04-29 06:48:37 +0530

    routes for user functionalities

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+const userRoute = require('express')()
+const userController = require('../controllers/userControllers'); // Importing user controller functions
+const userAuth = require('../middleware/userAuth'); // Importing user authentication middleware
+const upload = require('../middleware/multer'); // Importing middleware for handling file uploads
+
+// Define routes for user functionalities
+userRoute.post('/signup', userController.signup); // Route for user signup
+userRoute.post('/signin', userController.signin); // Route for user signin
+
+userRoute.post('/upload', userAuth, upload.single('file'), userController.upload); // Route for user file upload
+
+module.exports = userRoute
